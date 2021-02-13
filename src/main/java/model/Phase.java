6a82478d674fd2ea15e7cd7bb040464a8cb4503b
rahul patil <rahulpatil.m30@gmail.com>
2021-02-13 20:50:35 +0530

    added ISSUE and EXECUTE to Phases

--- src/main/java/model/Phase.java
+++ src/main/java/model/Phase.java
@@ -7,7 +7,7 @@
 public enum Phase {
 
     /**
-     * The game is yet to begin. First command has yet not been encountered.
+     * The game is yet to begin. Start of initial CLI for user.
      * Phase ends when 'editmap' or 'loadmap' command is encountered.
      */
     NULL,
@@ -21,6 +21,7 @@ public enum Phase {
     /**
      * Game is in start-up phase, i.e. players will be added/removed, countries will be distributed, and initial
      * armies will be assigned to the players.
+     * Phase usually set after the loadmap command.
      * Phase ends when 'assigncountries' command is encountered, i.e countries are distributed and initial armies are
      * assigned.
      */
@@ -30,7 +31,7 @@ public enum Phase {
      * Player assigns initial armies to the countries owned by him/her.
      * Phase ends when all player's have assigned initial armies.
      */
-    ARMYALLOCATION,
+    ASSIGN_REINFORCEMENTS,
 
     /**
      * Individual turn of player begins in round-robin fashion.
@@ -40,13 +41,26 @@ public enum Phase {
     DEPLOYMENT,
 
     /**
+     * Individual turn of player begins in round-robin fashion.
+     * Player adds order to the list of current orders as in deploy command.
+     * Phase ends all Players provide order based on individual turn.
+     */
+    ISSUE_ORDERS,
+
+    /**
+     * Executes orders for all the Players from the pool of order's list.
+     * Phase ends when all the num armies are placed on CuntryID as per order's list.
+     */
+    EXECUTE_ORDERS,
+
+    /**
      * Marks the end of the current player and signals to let the player get the turn.
      * Phase ends when next player starts their move.
      */
     TURNEND,
 
     /**
-     * Indicates desire to completely close the game.
+     * CLoses the Game.
      */
     QUIT
 }
