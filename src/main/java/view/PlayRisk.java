c2a6b70e3d83ce882b0c6db9617ed67e96585c85
rahul patil <rahulpatil.m30@gmail.com>
2021-02-16 12:15:23 +0530

    added JavaDoc to PlayRisk

--- src/main/java/view/PlayRisk.java
+++ src/main/java/view/PlayRisk.java
@@ -1,11 +1,11 @@
 package view;
 
-import controller.Command;
 import controller.GameEngine;
 import model.Phase;
 import model.Player;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 
 
@@ -28,32 +28,36 @@ public class PlayRisk {
         System.out.println("try, Selecting a map from the below mentioned sample maps or create a new one: ");
         l_game.sampleMaps();
 
+        //initial command reader from cli
         Scanner sc = new Scanner(System.in);
         String l_cmd = sc.nextLine();
+        //initial Phase set to NULL
         Phase l_gamePhase = Phase.NULL;
         GameEngine cmd = new GameEngine();
         l_gamePhase = cmd.parseCommand(null, l_cmd);
-        while(l_gamePhase!= Phase.ASSIGN_REINFORCEMENT) {
+        while(l_gamePhase!= Phase.EXECUTE_ORDERS) {
             l_cmd = sc.nextLine();
             l_gamePhase = cmd.parseCommand(null, l_cmd);
         }
 
+        //Loops through list of Players
         int l_numberOfPlayers = cmd.d_Players.size();
         int l_traversalCounter = 0;
         while(true) {
             while(l_traversalCounter<l_numberOfPlayers) {
                 Player l_p = cmd.d_Players.get(l_traversalCounter);
-                AssignReinforcement.assignReinforcementArmies(l_p);
+                //Assign to each player the correct number of reinforcement armies according to the Warzone rules.
+                //ASSIGN_REINFORCEMENTS.assignReinforcementArmies(l_p);
                 System.out.println("It's "+ l_p.getPlayerName() + "'s turn");
 
                 while(l_gamePhase!=Phase.TURNEND) {
-                    if(l_gamePhase==Phase.ASSIGN_REINFORCEMENT) {
-                        System.out.println("Reinforcement Armies: " + l_p.getOwnedArmies());
+                    if(l_gamePhase==Phase.DEPLOYMENT) {
+                        //System.out.println("Reinforcement Armies: " + l_p.getOwnedArmies());
                     }
                     l_cmd = sc.nextLine();
                     l_gamePhase = cmd.parseCommand(l_p, l_cmd);
                 }
-                l_gamePhase = Phase.ASSIGN_REINFORCEMENT;
+                l_gamePhase = Phase.DEPLOYMENT;
                 cmd.setGamePhase(l_gamePhase);
                 l_traversalCounter++;
             }
@@ -70,7 +74,7 @@ public class PlayRisk {
         File d_Folder = new File("src/main/resources/maps/");
         File[] d_Files = d_Folder.listFiles();
 
-        for(int i=0; i<d_Files.length; i++) {
+        for(int i = 0; i< Objects.requireNonNull(d_Files).length; i++) {
             if(d_Files[i].isFile())
                 System.out.print(d_Files[i].getName()+" /t");
         }
