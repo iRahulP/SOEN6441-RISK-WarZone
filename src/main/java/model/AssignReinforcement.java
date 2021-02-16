9b7a0a4c2420f9502babdccadc8bffdb4e14d77f
rahul patil <rahulpatil.m30@gmail.com>
2021-02-16 19:33:04 +0530

    created AssignReinforcement

---
+++ src/main/java/model/AssignReinforcement.java
@@ -0,0 +1,30 @@
+package model;
+
+/**
+ * Handles tasks related to Assign_Reinforcement Phase which assigns armies according to WarZone rules.
+ *
+ */
+public class AssignReinforcement {
+    /**
+     * Function assigns to each player the correct number of reinforcement armies according to the Warzone rules.
+     * @param p_player tracks current Player
+     * @return true if successfully assigned, else false
+     */
+    public static boolean assignReinforcementArmies(Player p_player){
+
+        return false;
+    }
+
+    /**
+     * deploy function allows Player to Place Armies by reducing the number of armies in the
+     * player’s reinforcement pool
+     * @param player tracks current Player
+     * @param countryId armies deployed here
+     * @param num unit of armies to be deployed
+     * @return true if successfully deployed, else false
+     */
+    public boolean deploy(Player player, String countryId, int num){
+
+        return false;
+    }
+}
