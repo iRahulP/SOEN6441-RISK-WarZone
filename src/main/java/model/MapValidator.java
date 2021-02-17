a6fa4dfe0e2afbcc16632ee95f88b92c01f48743
Shravyareddy14 <Shravyareddy14@users.noreply.github.com>
2021-02-17 09:46:45 +0530

    update in map validator

--- src/main/java/model/MapValidator.java
+++ src/main/java/model/MapValidator.java
@@ -6,10 +6,30 @@ package model;
  */
 public class MapValidator {
 
-	public static boolean doesContinentExist(GameMap map, String continentName) {
-		if(map.getContinents().containsKey(continentName.toLowerCase()))
+	/**
+	 * Check if the same continent already exist
+	 * @param p_map GameMap object holding record of all the existing continents and countries
+	 * @param p_continentName name of the continent to be checked
+	 * @return true if continent already exists, else false
+	 */
+	public static boolean doesContinentExist(GameMap p_map, String p_continentName) {
+		if(p_map.getContinents().containsKey(p_continentName.toLowerCase()))
 			return true;
 		else
 			return false;
 	}
+
+	/**
+	 * Check if the same country already exist
+	 * @param p_map GameMap object holding record of all the existing continents and countries
+	 * @param p_countryName name of the country to be checked
+	 * @return true if country already exists, else false
+	 */
+	public static boolean doesCountryExist(GameMap p_map, String p_countryName) {
+		if(p_map.getCountries().containsKey(p_countryName.toLowerCase()))
+			return true;
+		else
+			return false;
+	}
+
 }
