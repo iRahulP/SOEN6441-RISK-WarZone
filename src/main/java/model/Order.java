18a989c46d649ce221dcc518b6a70f12b593bb42
rahul patil <rahulpatil.m30@gmail.com>
2021-02-20 21:29:35 +0530

    initialized getter and setter for Order

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -27,4 +27,52 @@ public class Order {
         return true;
     }
 
+    /**
+     * Getter for current player
+     * @return d_player
+     */
+    public String getD_player() {
+        return d_player;
+    }
+
+    /**
+     * Setter for current player
+     * @param d_player player
+     */
+    public void setD_player(String d_player) {
+        this.d_player = d_player;
+    }
+
+    /**
+     * Getter for ID of Country
+     * @return d_countryId
+     */
+    public String getD_countryId() {
+        return d_countryId;
+    }
+
+    /**
+     * Setter for ID of Country
+     * @param d_countryId country ID
+     */
+    public void setD_countryId(String d_countryId) {
+        this.d_countryId = d_countryId;
+    }
+
+    /**
+     * Setter for number of armies
+     * @return d_numArmies
+     */
+    public int getD_numArmies() {
+        return d_numArmies;
+    }
+
+    /**
+     * Setter for number of armies
+     * @param d_numArmies number of armies
+     */
+    public void setD_numArmies(int d_numArmies) {
+        this.d_numArmies = d_numArmies;
+    }
+
 }
