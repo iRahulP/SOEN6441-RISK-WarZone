da3d1f9c2d1314fcec1990c630340b735f012ddd
rahul patil <rahulpatil.m30@gmail.com>
2021-02-21 13:44:42 +0530

    updated Order Class

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,78 @@
+package model;
+
+public class Order {
+
+    private String d_player;
+    private int d_numArmies;
+    private String d_countryId;
+
+    /**
+     * This constructor will initialize the order object with deploy details.
+     * @param p_player current player issuing deploy order
+     * @param p_countryId country where armies will be deployed
+     * @param p_numArmies total armies which will be deployed
+     */
+    public Order(String p_player,String p_countryId,int p_numArmies) {
+        d_player = p_player;
+        d_countryId = p_countryId;
+        d_numArmies = p_numArmies;
+    }
+
+    /**
+     * method which enacts the order
+     * @return true if successful, else false
+     */
+    public boolean execute(){
+
+        return true;
+    }
+
+    /**
+     * Getter for current player
+     * @return d_player
+     */
+    public String getD_player() {
+        return d_player;
+    }
+
+    /**
+     * Setter for current player
+     * @param d_player player
+     */
+    public void setD_player(String d_player) {
+        this.d_player = d_player;
+    }
+
+    /**
+     * Getter for ID of Country
+     * @return d_countryId
+     */
+    public String getD_countryId() {
+        return d_countryId;
+    }
+
+    /**
+     * Setter for ID of Country
+     * @param d_countryId country ID
+     */
+    public void setD_countryId(String d_countryId) {
+        this.d_countryId = d_countryId;
+    }
+
+    /**
+     * Setter for number of armies
+     * @return d_numArmies
+     */
+    public int getD_numArmies() {
+        return d_numArmies;
+    }
+
+    /**
+     * Setter for number of armies
+     * @param d_numArmies number of armies
+     */
+    public void setD_numArmies(int d_numArmies) {
+        this.d_numArmies = d_numArmies;
+    }
+
+}
