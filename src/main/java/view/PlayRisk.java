7ea35ffee765ec0a28f6492a1146d7f8187acc23
rahul patil <rahulpatil.m30@gmail.com>
2021-02-21 14:41:40 +0530

    PlayRisk Updated

--- src/main/java/view/PlayRisk.java
+++ src/main/java/view/PlayRisk.java
@@ -28,7 +28,6 @@ public class PlayRisk {
         System.out.println("Welcome to Risk Game based on Warzone!");
         System.out.println("try, Selecting a map from the below mentioned sample maps or create a new one: ");
         l_game.sampleMaps();
-
         //initial command reader from cli
         Scanner sc = new Scanner(System.in);
         String l_cmd = sc.nextLine();
@@ -37,36 +36,35 @@ public class PlayRisk {
         GameEngine cmd = new GameEngine();
         l_gamePhase = cmd.parseCommand(null, l_cmd);
         //looping for commands until initial Phases where Player iteration not required!
-        while(l_gamePhase!= Phase.ASSIGN_REINFORCEMENTS) {
+        while(l_gamePhase!= Phase.DEPLOYMENT) {
             l_cmd = sc.nextLine();
             l_gamePhase = cmd.parseCommand(null, l_cmd);
         }
 
         //Loops through list of Players
         int l_numberOfPlayers = cmd.d_Players.size();
-        int l_traversalCounter = 0;
-        while(true) {
-            while(l_traversalCounter<l_numberOfPlayers) {
-                Player l_p = cmd.d_Players.get(l_traversalCounter);
+        int l_playerCounter = 0;
+        while(true){
+            while(l_playerCounter<l_numberOfPlayers) {
+                Player l_p = cmd.d_Players.get(l_playerCounter);
                 //Assign to each player the correct number of reinforcement armies according to the Warzone rules.
                 AssignReinforcement.assignReinforcementArmies(l_p);
                 System.out.println("It's "+ l_p.getPlayerName() + "'s turn");
-
-                while(l_gamePhase!=Phase.TURNEND) {
-                    if(l_gamePhase==Phase.DEPLOYMENT) {
-                        //System.out.println("Reinforcement Armies: " + l_p.getOwnedArmies());
-                    }
+                while(l_gamePhase!=Phase.TURN) {
+                    //
+                    System.out.println("Still in loop");
                     l_cmd = sc.nextLine();
                     l_gamePhase = cmd.parseCommand(l_p, l_cmd);
                 }
-                l_gamePhase = Phase.EXECUTE_ORDERS;
+                System.out.print("Out of TURN Phase");
                 l_gamePhase = Phase.DEPLOYMENT;
                 cmd.setGamePhase(l_gamePhase);
-                l_traversalCounter++;
+                l_playerCounter++;
             }
-            l_traversalCounter = 0;
+            l_gamePhase = Phase.EXECUTE_ORDERS;
+            cmd.setGamePhase(l_gamePhase);
+            l_playerCounter = 0;
         }
-
     }
 
     /**
