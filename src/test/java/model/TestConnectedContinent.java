78ede4170c08627ce9eabc03d45d008577efc30d
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-02-23 20:59:53 +0530

    Test ContinentConnectivity

---
+++ src/test/java/model/TestConnectedContinent.java
@@ -0,0 +1,45 @@
+package model;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests if connected continent are being rightly identified or not.
+ *
+ */
+public class TestConnectedContinent {
+    MapValidator d_Mvr;
+    RunGameEngine d_Rgame;
+    GameMap d_Map;
+
+    /**
+     * Set up the context
+     */
+    @Before
+    public void before(){
+        d_Mvr = new MapValidator();
+        d_Rgame = new RunGameEngine();
+        d_Map = new GameMap("world.map");
+    }
+
+    /**
+     * Test if tests are rightly identified or not
+     */
+    @Test
+    public void testConnectedContinent(){
+        d_Map = d_Rgame.editMap("world.map");
+        boolean check = d_Mvr.continentConnectivityCheck(d_Map);
+        assertTrue(check);
+    }
+
+    @Test
+    public void testNotConnectedContinent(){
+        d_Map = d_Rgame.editMap("createdMap.map");
+        boolean check = d_Mvr.continentConnectivityCheck(d_Map);
+        assertTrue(check);
+    }
+
+
+}
