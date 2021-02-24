f8b2171ede13dce1e62c1fb3a48fb4766380fdd2
rahul patil <rahulpatil.m30@gmail.com>
2021-02-24 12:12:49 +0530

    Created Test Suite for collection of all Tests

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,27 @@
+import model.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        TestAddOrder.class,
+        TestAssigncountries.class,
+        TestConnectedContinent.class,
+        TestContinent.class,
+        TestCountryDetails.class,
+        TestEditMap.class,
+        TestNeighbor.class,
+        TestRemoveOrder.class,
+        TestValidateMap.class
+})
+
+/**
+ *  It runs a collection of test cases.
+ */
+public class TestSuite {
+    //Driver for All Test Cases
+}
+
+
