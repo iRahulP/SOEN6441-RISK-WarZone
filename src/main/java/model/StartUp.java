6d16a260dd7fae84ad2969ff7925136d81bf246f
yashs <yashs@LAPTOP-ORKMKVKU>
2021-02-24 14:08:29 +0530

    StartUp class final commit

--- src/main/java/model/StartUp.java
+++ src/main/java/model/StartUp.java
@@ -36,9 +36,9 @@ public class StartUp {
 	public boolean removePlayer(ArrayList<Player> p_players, String p_playerName){
 		Iterator<Player> itr = p_players.listIterator();
 		while(itr.hasNext()) {
-			Player p = itr.next();
-			if(p.getPlayerName().equals(p_playerName)) {
-				p_players.remove(p);
+			Player l_p = itr.next();
+			if(l_p.getPlayerName().equals(p_playerName)) {
+				p_players.remove(l_p);
 				return true;
 			}
 		}
@@ -52,21 +52,20 @@ public class StartUp {
 	 * @return true if successful, else false
 	 */
 	public boolean assignCountries(GameMap map, ArrayList<Player> players) {
-		int numberOfPlayers = players.size();
+		int l_numberOfPlayers = players.size();
 		if(players.size()<2) {
 			System.out.println("Minimum two players are required to play the game.");
 			return false;
 		}
-		int counter = 0;
+		int l_counter = 0;
 		for(CountryDetails c : map.getCountries().values()) {
-			Player p = players.get(counter);
-			p.getOwnedCountries().put(c.getCountryId().toLowerCase(), c);
-			if(counter>=numberOfPlayers-1)
-				counter = 0;
+			Player l_p = players.get(l_counter);
+			l_p.getOwnedCountries().put(c.getCountryId().toLowerCase(), c);
+			if(l_counter>=l_numberOfPlayers-1)
+				l_counter = 0;
 			else
-				counter++;
+				l_counter++;
 		}
-		//assignInitialArmies(players);  //assigning initial armies
 		return true;
 	}
 
@@ -79,8 +78,8 @@ public class StartUp {
 		if(p_map==null)
 			return;
 		if(p_players.size()==0 || p_players.get(0).getOwnedCountries().size()==0) {
-			RunGameEngine rc = new RunGameEngine();
-			rc.showMap(p_map);
+			RunGameEngine l_rc = new RunGameEngine();
+			l_rc.showMap(p_map);
 			return;
 		}
 		System.out.format("%25s%25s%35s%25s%10s\n", "Owner", "Country", "Neighbors", "Continent", "#Armies");
@@ -91,24 +90,24 @@ public class StartUp {
 		boolean l_printNumberOfArmies = true;
 
 		for(int i=0; i<p_players.size(); i++){
-			Player p = p_players.get(i);
-			for(CountryDetails country : p.getOwnedCountries().values()) {
-				for(CountryDetails neighbor : country.getNeighbours().values()) {
+			Player l_p = p_players.get(i);
+			for(CountryDetails l_country : l_p.getOwnedCountries().values()) {
+				for(CountryDetails l_neighbor : l_country.getNeighbours().values()) {
 					if(l_printPlayerName && l_printContinentId && l_printCountryId) {
-						System.out.format("\n%25s%25s%35s%25s%10d\n", p.getPlayerName(), country.getCountryId(), neighbor.getCountryId(), country.getInContinent(), country.getNumberOfArmies());
+						System.out.format("\n%25s%25s%35s%25s%10d\n", l_p.getPlayerName(), l_country.getCountryId(), l_neighbor.getCountryId(), l_country.getInContinent(), l_country.getNumberOfArmies());
 						l_printPlayerName = false;
 						l_printContinentId = false;
 						l_printCountryId = false;
 						l_printNumberOfArmies = false;
 					}
 					else if(l_printContinentId && l_printCountryId && l_printNumberOfArmies) {
-						System.out.format("\n%25s%25s%35s%25s%10d\n", "", country.getCountryId(), neighbor.getCountryId(), country.getInContinent(), country.getNumberOfArmies());
+						System.out.format("\n%25s%25s%35s%25s%10d\n", "", l_country.getCountryId(), l_neighbor.getCountryId(), l_country.getInContinent(), l_country.getNumberOfArmies());
 						l_printPlayerName = false;
 						l_printCountryId = false;
 						l_printNumberOfArmies = false;
 					}
 					else {
-						System.out.format("\n%25s%25s%35s%25s%10s\n", "", "", neighbor.getCountryId(), "", "");
+						System.out.format("\n%25s%25s%35s%25s%10s\n", "", "", l_neighbor.getCountryId(), "", "");
 					}
 				}
 				l_printContinentId = true;
