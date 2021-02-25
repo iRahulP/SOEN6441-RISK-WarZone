f865611b7687d36886e35d93cd77f9f74c772ad5
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-02-25 12:09:26 +0530

    comments

--- src/test/java/model/TestOrderCapability.java
+++ src/test/java/model/TestOrderCapability.java
@@ -51,24 +51,24 @@ public class TestOrderCapability {
         d_Stup = new StartUp();
         d_Ge = new GameEngine();
         d_Map = d_Rge.loadMap("dummy.map");
-        boolean check = d_Stup.assignCountries(d_Map, d_Players);
+        boolean l_check = d_Stup.assignCountries(d_Map, d_Players);
         System.out.println(d_Player1.getOwnedCountries());
         System.out.println("Countries assigned to "+d_Player1.getPlayerName()+" : "+d_Player1.getOwnedCountries());
         System.out.println("Countries assigned to "+d_Player2.getPlayerName()+" : "+d_Player2.getOwnedCountries());
 
         //only assigned to Player 1
-        if(check){
+        if(l_check){
             AssignReinforcement.assignReinforcementArmies(d_Player1);
         }
         //performed checks for owned country and allowed army units.
-        boolean checkOwnedCountry1 = d_Player1.getOwnedCountries().containsKey("japan");
-        boolean checkArmies1 = (d_Player1.getOwnedArmies() >= d_NumberOfArmies);
+        boolean l_checkOwnedCountry1 = d_Player1.getOwnedCountries().containsKey("japan");
+        boolean l_checkArmies1 = (d_Player1.getOwnedArmies() >= d_NumberOfArmies);
 
-        if(checkOwnedCountry1 && checkArmies1){
+        if(l_checkOwnedCountry1 && l_checkArmies1){
 
             System.out.println("Before :"+d_Player1.getOwnedArmies());
-            Order temp = new Order(d_Player1, "japan", d_NumberOfArmies);
-            d_Player1.addOrder(temp);
+            Order l_temp = new Order(d_Player1, "japan", d_NumberOfArmies);
+            d_Player1.addOrder(l_temp);
             d_Player1.issue_order();
             d_Player1.setOwnedArmies(d_Player1.getOwnedArmies()-d_NumberOfArmies);
             System.out.println("After :"+d_Player1.getOwnedArmies());
@@ -85,8 +85,8 @@ public class TestOrderCapability {
 
         if(checkOwnedCountry2 && checkArmies2){
             System.out.println("Before :"+d_Player2.getOwnedArmies());
-            Order temp = new Order(d_Player2, "sluci", d_NumberOfArmies);
-            d_Player2.addOrder(temp);
+            Order l_temp = new Order(d_Player2, "sluci", d_NumberOfArmies);
+            d_Player2.addOrder(l_temp);
             d_Player2.issue_order();
             d_Player2.setOwnedArmies(d_Player2.getOwnedArmies()-d_NumberOfArmies);
             System.out.println("After :"+d_Player2.getOwnedArmies());
