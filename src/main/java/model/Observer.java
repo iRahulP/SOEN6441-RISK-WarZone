9bac0b898bdca30de3be04c4e5422361d314085b
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-03-20 09:38:17 +0530

    Observer class changes

---
+++ src/main/java/model/Observer.java
@@ -0,0 +1,15 @@
+package model;
+
+import model.Observable;
+
+/**
+ * Updates the observing classes on getting notification from Observable.
+ */
+public interface Observer {
+    /**
+     * Function carrying out update work in response to some observed action.
+     * @param p_observer Observable object
+     */
+    public void update(Observable p_observer);
+
+}
