2c6aeba6a3177f3593f2acb89d009443a5155051
rahul patil <rahulpatil.m30@gmail.com>
2021-03-20 13:54:57 +0530

    updated Order as an Interface

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -5,83 +5,8 @@ import java.util.Iterator;
 /**
  * class performs execute orders functionality 
  */
-public class Order {
+public interface Order {
 
-    private int d_NumArmies;
-    private String d_CountryId;
-    private Player d_Player;
-
-  
-    /**
-     * This constructor will initialize the order object with deploy details.
-     * @param p_player current player issuing deploy order
-     * @param p_countryId country where armies will be deployed
-     * @param p_numArmies total armies which will be deployed
-     */
-    public Order(Player p_player,String p_countryId,int p_numArmies) {
-        d_Player = p_player;
-        d_CountryId = p_countryId;
-        d_NumArmies = p_numArmies;
-    }
-
-    /**
-     * method which enacts the order
-     * @return true if successful, else false
-     */
-    public boolean execute(){
-        CountryDetails l_c= d_Player.getOwnedCountries().get(d_CountryId.toLowerCase());
-        int l_existingArmies = l_c.getNumberOfArmies();
-        l_existingArmies += d_NumArmies;
-        l_c.setNumberOfArmies(l_existingArmies);
-        return true;
-    }
-
-    /**
-     * Getter for current player
-     * @return d_player
-     */
-    public Player getD_player() {
-        return d_Player;
-    }
-
-    /**
-     * Setter for current player
-     * @param d_player player
-     */
-    public void setD_player(Player d_player) {
-        this.d_Player = d_player;
-    }
-
-    /**
-     * Getter for ID of Country
-     * @return d_countryId
-     */
-    public String getD_countryId() {
-        return d_CountryId;
-    }
-
-    /**
-     * Setter for ID of Country
-     * @param d_countryId country ID
-     */
-    public void setD_countryId(String d_countryId) {
-        this.d_CountryId = d_countryId;
-    }
-
-    /**
-     * Setter for number of armies
-     * @return d_numArmies
-     */
-    public int getD_numArmies() {
-        return d_NumArmies;
-    }
-
-    /**
-     * Setter for number of armies
-     * @param d_numArmies number of armies
-     */
-    public void setD_numArmies(int d_numArmies) {
-        this.d_NumArmies = d_numArmies;
-    }
+public boolean execute();
 
 }
