baa239791035708d3ecb113c4b067c0ce192b92a
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-03-21 23:14:13 +0530

    Play Phase changes

---
+++ src/main/java/model/Play.java
@@ -0,0 +1,17 @@
+package model;
+
+public abstract class Play extends Phase{
+
+	@Override
+	public void loadMap(String[] p_data, String p_mapName) {
+		printInvalidCommandMessage();
+		
+	}
+
+
+	@Override
+	public void showMap(GameMap p_map) {
+		// TODO Auto-generated method stub
+		
+	}
+}
