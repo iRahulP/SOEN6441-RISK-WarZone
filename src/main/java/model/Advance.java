3176cff9a901144752ca064dcbf4af04298de8a2
rucha1412 <rucha.prajapati98@gmail.com>
2021-03-21 23:23:21 +0530

    implementation of advance order

--- src/main/java/model/Advance.java
+++ src/main/java/model/Advance.java
@@ -1,10 +1,111 @@
 package model;
 
+
 public class Advance implements Order{
 
+	private int d_NumArmies;
+	private String d_SourceCountryId, d_TargetCountryId;
+	private Player d_Player;
+	 
+	public Advance(Player p_player,String p_sourceCountryId,String p_targetCountryId,int p_numArmies){
+		d_Player= p_player;
+		d_SourceCountryId= p_sourceCountryId;
+		d_TargetCountryId= p_targetCountryId;
+		d_NumArmies= p_numArmies;
+	}
 	@Override
 	public boolean execute() {
-		return false;
+		
+		if(d_Player.getOwnedCountries().containsKey(d_SourceCountryId.toLowerCase())){
+			if(d_Player.getOwnedCountries().containsKey(d_TargetCountryId.toLowerCase())){
+				if((d_Player.getOwnedCountries().get(d_SourceCountryId.toLowerCase()).getNumberOfArmies()) > d_NumArmies){
+						int fromArmies = d_Player.getOwnedCountries().get(d_SourceCountryId.toLowerCase()).getNumberOfArmies();
+						fromArmies -= d_NumArmies;
+						d_Player.getOwnedCountries().get(d_SourceCountryId.toLowerCase()).setNumberOfArmies(fromArmies);
+						int toArmies = d_Player.getOwnedCountries().get(d_TargetCountryId.toLowerCase()).getNumberOfArmies();
+						toArmies += d_NumArmies;
+						d_Player.getOwnedCountries().get(d_TargetCountryId.toLowerCase()).setNumberOfArmies(toArmies);
+						return true;
+				}
+				else{
+					System.out.println("player don't have enough armies.");
+					return false;
+				}
+			}
+			else{
+				System.out.println(d_TargetCountryId + " is not owned by the player");
+				System.out.println("Attack Occur between: "+ d_TargetCountryId+ " and "+ d_SourceCountryId);
+				//d_Player.performAttack(d_SourceCountryId, d_TargetCountryId);
+				return true;
+			}
+		}
+		else {
+			System.out.println(d_SourceCountryId + " is not owned by the player");
+			return false;
+		}
 	}
 
+	 /**
+     * Getter for current player
+     * @return d_player
+     */
+    public Player getD_player() {
+        return d_Player;
+    }
+
+    /**
+     * Setter for current player
+     * @param p_player player
+     */
+    public void setD_player(Player p_player) {
+        this.d_Player = p_player;
+    }
+
+    /**
+     * Getter for ID of Source Country
+     * @return d_SourceCountryId
+     */
+    public String getD_sourceCountryId() {
+        return d_SourceCountryId;
+    }
+
+    /**
+     * Setter for ID of source Country
+     * @param p_sourceCountryId source country ID 
+     */
+    public void setD_sourceCountryId(String p_sourceCountryId) {
+        this.d_SourceCountryId = p_sourceCountryId;
+    }
+    
+    /**
+     * Getter for ID of Target Country
+     * @return d_TargetCountryId
+     */
+    public String getD_targetCountryId() {
+        return d_TargetCountryId;
+    }
+
+    /**
+     * Setter for ID of Target Country
+     * @param p_targetCountryId country ID
+     */
+    public void setD_targetCountryId(String p_targetCountryId) {
+        this.d_SourceCountryId = p_targetCountryId;
+    }
+
+    /**
+     * Setter for number of armies
+     * @return d_numArmies
+     */
+    public int getD_numArmies() {
+        return d_NumArmies;
+    }
+
+    /**
+     * Setter for number of armies
+     * @param d_numArmies number of armies
+     */
+    public void setD_numArmies(int d_numArmies) {
+        this.d_NumArmies = d_numArmies;
+    }
 }
