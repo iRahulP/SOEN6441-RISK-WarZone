cf529939e4ad2ebc0df72f1f36fc4ab25db02d16
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-03-23 09:26:17 +0530

    Card class changes

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Card {
+
+    String d_CardType;
+    String d_Card;
+    String[] cardsList = {"Bomb","Airlift","Blockade","Diplomacy"};
+
+
+    String getCardType()
+    {
+        return d_CardType;
+    }
+
+    void createCard()
+    {
+        d_CardType = randomCard();
+    }
+
+    String randomCard()
+    {
+
+        Random randomGenerator = new Random();
+        int index = randomGenerator.nextInt(cardsList.length);
+        return cardsList[index];
+    }
+
+}
