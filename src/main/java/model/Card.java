19f1dc71cdb1e55eed9ae7c5c908e6b3e5121f1b
Shravyareddy14 <Shravyareddy14@users.noreply.github.com>
2021-03-23 09:46:27 +0530

    Update in Card

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Card {
     String d_CardType;
     String d_Card;
-    String[] cardsList = {"Bomb","Airlift","Blockade","Diplomacy"};
+    String[] d_CardsList = {"Bomb","Airlift","Blockade","Diplomacy"};
 
 
     String getCardType()
@@ -22,7 +22,7 @@ public class Card {
     {
 
         Random randomGenerator = new Random();
-        int index = randomGenerator.nextInt(cardsList.length);
-        return cardsList[index];
+        int index = randomGenerator.nextInt(d_CardsList.length);
+        return d_CardsList[index];
     }
 }
