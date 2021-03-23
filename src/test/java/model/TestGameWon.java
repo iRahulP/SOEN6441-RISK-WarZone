e3d4fd7857f8e840bcb65b924fe4a007e11b0a2a
iRahulP <rahulpatil.m30@gmail.com>
2021-03-23 11:55:38 +0530

    test if game won by a specific player

---
+++ src/test/java/model/TestGameWon.java
@@ -0,0 +1,71 @@
+package model;
+
+import controller.GameEngine;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Queue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestGameWon{
+    Player d_Player1;
+    Player d_Player2;
+    GameMap d_Map;
+    ArrayList<Player> d_Players;
+    StartUp d_Stup;
+    InternalPhase d_GamePhase;
+    GameEngine d_Ge;
+    RunGameEngine d_Rge;
+    String d_CountryId = null;
+    boolean l_checkOwnedCountry;
+    /**
+     * initial setup
+     */
+    @Before
+    public void before() {
+        d_Player1 = new Player("Spidey");
+        d_Player2 = new Player("WonderWoman");
+        d_Map = new GameMap("dummy.map");
+        d_Rge = new RunGameEngine();
+        d_Players = new ArrayList<Player>();
+        d_Players.add(d_Player1);
+        d_Players.add(d_Player2);
+        d_GamePhase = InternalPhase.ISSUE_ORDERS;
+        l_checkOwnedCountry = true;
+    }
+
+    /**
+     * Test limits of Player's orders, with checks for country owned and army units.
+     * Sample reinforcements assigned for Player1 and tested with unassigned reinforcements for Player2
+     */
+    @Test
+    public void testBlockadeEffect() {
+        d_Stup = new StartUp(d_Ge);
+        d_Ge = new GameEngine();
+        d_Map = d_Rge.loadMap("dummy.map");
+        d_Stup.assignCountries(d_Map, d_Players);
+        System.out.println("Countries assigned to "+d_Player1.getPlayerName()+" : "+d_Player1.getOwnedCountries());
+        System.out.println("Countries assigned to "+d_Player2.getPlayerName()+" : "+d_Player2.getOwnedCountries());
+        d_Player2.getOwnedCountries().putAll(d_Player1.getOwnedCountries());
+        d_Player1.getOwnedCountries().clear();
+        System.out.println("Countries assigned to "+d_Player1.getPlayerName()+" : "+d_Player1.getOwnedCountries());
+        System.out.println("Countries assigned to "+d_Player2.getPlayerName()+" : "+d_Player2.getOwnedCountries());
+
+        boolean ifWon = false;
+        for (Player l_p : d_Players){
+            if(l_p.getOwnedCountries().size() == d_Map.getCountries().size()){
+                System.out.println(l_p.getPlayerName()+" has Won the Game!");
+                ifWon = true;
+            }
+        }
+
+        assertTrue(ifWon);
+    }
+
+}
