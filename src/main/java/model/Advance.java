f2b5445708586e711acb183c2c4960f76433e420
iRahulP <rahulpatil.m30@gmail.com>
2021-03-23 13:32:30 +0530

    updated Advance as null passed to addCard

--- src/main/java/model/Advance.java
+++ src/main/java/model/Advance.java
@@ -70,7 +70,13 @@ public class Advance implements Order{
 					d_Player.getOwnedCountries().put(d_TargetCountryId, defendingCountry);
 					defendingCountry.setNumberOfArmies(attackerArmyLeft);
 					attackingCountry.setNumberOfArmies(1);
-					d_Player.addCard();
+					//If Attack Successful and new territory added to Player
+					//Generate a random Card from {'BOMB', 'AIRLIFT', 'BLOCKADE', 'DIPLOMACY'}
+					//Card d_Card;
+					//String d_CardType;
+					//d_CardType="Bomb"; for example
+					//d_Card=new Card(d_CardType);
+					//d_Player.addCard();
 				}
 				//if defending coutry has armies
 				else {
