66fa38b9f3995aaf821c698c2d5931adf4571c31
Shravyareddy14 <Shravyareddy14@users.noreply.github.com>
2021-03-23 19:32:41 +0530

    Initial Commit to Diplomacy

---
+++ src/main/java/model/Diplomacy.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Diplomacy implements Order{
+
+    private Player d_currentPlayer, d_targetPlayer;
+
+    public Diplomacy(Player p_currentPlayer,Player p_targetPlayer){
+        this.d_currentPlayer = p_currentPlayer;
+        this.d_targetPlayer = p_targetPlayer;
+    }
+
+    @Override
+    public boolean execute() {
+        this.d_currentPlayer.addPlayerNegList(d_targetPlayer);
+        this.d_targetPlayer.addPlayerNegList(d_currentPlayer);
+        return true;
+    }
+}
