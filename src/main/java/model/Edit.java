f84265db5195bf6347ac0465a46513d5963a629a
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-03-24 21:28:58 +0530

    setMessage changes

--- src/main/java/model/Edit.java
+++ src/main/java/model/Edit.java
@@ -8,16 +8,22 @@ public abstract class Edit extends Phase{
 	
 	public void reinforce() {
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 	}
+
     public void gamePlayer(String[] p_data, ArrayList<Player> p_players, String p_playerName) {
     	printInvalidCommandMessage();
-
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
     }
+
 	public boolean assignCountries(GameMap p_map, ArrayList<Player> p_players) {
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 		return false;
 	}
+
     public void showMap(ArrayList<Player> p_players, GameMap p_map) {
 	    	printInvalidCommandMessage();
-	    }
+     		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
+	}
   }
