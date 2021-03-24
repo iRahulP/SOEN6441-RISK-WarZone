4119fdc0dd04684f08a23619d45978c9fb8a27ce
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-03-24 21:30:20 +0530

    setMessage changes

--- src/main/java/model/PostLoad.java
+++ src/main/java/model/PostLoad.java
@@ -70,36 +70,43 @@ public class PostLoad extends Edit {
 	@Override
 	public void loadMap(String[] p_data, String p_mapName) {
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 	}
 
 	@Override
 	public void editContinent(String[] p_data, String p_continentId, int p_controlValue) {
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 	}
 
 	@Override
 	public void editCountry(String[] p_data, String p_continentId, String p_countryId) {
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 	}
 
 	@Override
 	public void editNeighbour(String[] p_data, String p_countryId, String p_neighborCountryId) {
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 	}
 	
 	@Override
 	public void savemap(String[] p_data, String p_mapName) {
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 	}
 
 	@Override
 	public void editMap(String[] p_data, String p_mapName) {
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 	}
 
 	@Override
 	public void validatemap(){
 		printInvalidCommandMessage();
+		d_Ge.d_LogEntry.setMessage("Invalid command in phase "+d_Ge.d_Phase.getD_PhaseName());
 	}
 
 }
