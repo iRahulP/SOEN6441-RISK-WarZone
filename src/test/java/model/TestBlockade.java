ecbdd6c54e50d2c4bc590b49fa35b7f305978f7e
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-03-24 15:07:04 +0530

    tests changes due to logEntry

--- src/test/java/model/TestBlockade.java
+++ src/test/java/model/TestBlockade.java
@@ -55,8 +55,8 @@ public class TestBlockade{
      */
     @Test
     public void testBlockadeEffect() {
-        d_Stup = new StartUp(d_Ge);
         d_Ge = new GameEngine();
+        d_Stup = new StartUp(d_Ge);
         d_Map = d_Rge.loadMap("dummy.map");
         d_Stup.assignCountries(d_Map, d_Players);
         AssignReinforcement.assignReinforcementArmies(d_Player1);
