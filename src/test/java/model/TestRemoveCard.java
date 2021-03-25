12b920fe4f95f76fcac448492ac6c0f09ff836b8
Shravyareddy14 <Shravyareddy14@users.noreply.github.com>
2021-03-25 12:05:31 +0530

    Update for doesCardExist test

--- src/test/java/model/TestRemoveCard.java
+++ src/test/java/model/TestRemoveCard.java
@@ -3,11 +3,14 @@ package model;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
- * Test Class to checks wether card is removed or not.
+ * Test Class to checks whether card exist and is removed or not.
  */
 public class TestRemoveCard {
     Player d_player;
+    String d_Card;
 
     /**
      * Creating the player object
@@ -15,16 +18,19 @@ public class TestRemoveCard {
     @Before
     public void before(){
         d_player = new Player("Jupiter");
+        d_Card="Bomb";
     }
 
     /**
-     * To check the card is removed or not
+     * To check the card exist and is removed or not
      */
     @Test
     public void TestCard(){
         d_player.addCard("Bomb");
         d_player.addCard("Airlift");
         d_player.addCard("Diplomacy");
+        Boolean l_cardExist=d_player.doesCardExists(d_Card);
+        assertEquals(true,l_cardExist);
         d_player.showCards();
         d_player.removeCard("Bomb");
         System.out.println("After Removing Bomb card from the Deck");
