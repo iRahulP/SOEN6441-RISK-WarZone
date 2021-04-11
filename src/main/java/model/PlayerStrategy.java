8a8a5096dea03370c33c0cce22c3ea094bc07d3a
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-04-12 01:49:17 +0530

    abstract methods added

--- src/main/java/model/PlayerStrategy.java
+++ src/main/java/model/PlayerStrategy.java
@@ -4,8 +4,8 @@ package model;
  * PlayerStrategy acts as Strategy
  */
 public abstract class PlayerStrategy {
-    GameMap d_map;
-    Player d_player;
+    GameMap d_Map;
+    Player d_Player;
 
     /**
      * default Constructor for PlayerStrategy
@@ -13,11 +13,27 @@ public abstract class PlayerStrategy {
      * @param p_map map object
      */
     PlayerStrategy(Player p_player, GameMap p_map){
-        d_player = p_player;
-        d_map = p_map;
+        d_Player = p_player;
+        d_Map = p_map;
     }
+
     /**
      * method to create Orders based on specific Strategy for player
      */
     public abstract Order createOrder();
+
+    /**
+     *method to return the Country from where attack takes place
+     */
+    protected abstract CountryDetails toAttackFrom();
+
+    /**
+     *method to return the Country on which attack happens
+     */
+    protected abstract CountryDetails toAttack();
+
+    /**
+     *method to return Country to move armies from
+     */
+    protected abstract CountryDetails toMoveFrom();
 }
