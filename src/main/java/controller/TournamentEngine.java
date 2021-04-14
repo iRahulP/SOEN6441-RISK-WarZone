31bc03f8c81e41f441a26ed7596cd55ee934f93e
rahul patil <rahulpatil.m30@gmail.com>
2021-04-14 14:37:31 +0530

    updated tournamentMode engine

--- src/main/java/controller/TournamentEngine.java
+++ src/main/java/controller/TournamentEngine.java
@@ -1,10 +1,13 @@
 package controller;
 
 import model.*;
+import view.TournamentResultView;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Queue;
 
 /**
  * Manages Tournament
@@ -106,6 +109,8 @@ public class TournamentEngine extends GameEngine{
 
                                             if (n >= 10 && n <= 50) {
                                                 noOfTurns = Integer.parseInt(data[newIndex + 1]);
+                                                //tournament -M dummy.map ameroki.map -P benevolent random  -G 3 -D 150
+                                                System.out.println("Can Play");
                                                 //playTournament(maps, strategies, noOfGames, noOfTurns);
                                                 return "success";
                                             } else {
@@ -162,9 +167,6 @@ public class TournamentEngine extends GameEngine{
         int numberOfPlayers = strategies.size();
         int traversalCounter = 0;
         int gameNumber = 0;
-
-        GameEngine d_Ge;
-        StartUp d_Stup;
         HashMap<Integer, String> winner = new HashMap<Integer, String>();
 
         //Start playing on each map
@@ -175,44 +177,127 @@ public class TournamentEngine extends GameEngine{
 
                 //load the map
                 d_Ge = new GameEngine();
-                d_Stup = new StartUp(d_Ge);
                 d_Map = d_RunG.loadMap(mapName);
 
                 //Create player objects
                 for (String strategy : strategies) {
-                    d_Stup.addPlayer(d_Players, strategy);
+                    d_StartUp.addPlayer(d_Players, strategy);
                 }
 
                 //Setting strategies as same as Player Names
                 for (Player l_p : d_Players) {
                     switch (l_p.getPlayerName().toLowerCase()) {
                         case "aggressive":
-                            l_p.setStrategy(new AggresivePlayer(l_p, d_Ge.d_Map));
+                            l_p.setStrategy(new AggresivePlayer(l_p, d_Map));
                             l_p.setD_isHuman(false);
                             break;
                         case "benevolent":
-                            l_p.setStrategy(new BenevolentPlayer(l_p, d_Ge.d_Map));
+                            l_p.setStrategy(new BenevolentPlayer(l_p,d_Map));
                             l_p.setD_isHuman(false);
                             break;
                         case "random":
-                            l_p.setStrategy(new RandomPlayer(l_p, d_Ge.d_Map));
+                            l_p.setStrategy(new RandomPlayer(l_p, d_Map));
                             l_p.setD_isHuman(false);
                             break;
                         case "cheater":
-                            l_p.setStrategy(new CheaterPlayer(l_p, d_Ge.d_Map));
+                            l_p.setStrategy(new CheaterPlayer(l_p, d_Map));
                             l_p.setD_isHuman(false);
                             break;
                         default:
-                            l_p.setD_isHuman(true);
+                            System.out.println("Invalid Player Strategy");
                             break;
                     }
                 }
 
-                d_Stup.assignCountries(d_Map, d_Players);
+                //AssignCountries and Reinforcements
+                d_StartUp.assignCountries(d_Map, d_Players);
+                assignEachPlayerReinforcements(d_Ge);
 
-                //rest of Implementation remains
+                //For D number of Turns iterate Players to get Orders
+                for(int j=1; j<=numberOfTurns; j++){
+
+                    int l_counter = 0;
+                    //traverses through all players to check if armies left in pool
+                    Iterator<Player> l_itr = d_Players.listIterator();
+                    while(l_itr.hasNext()) {
+                        Player l_p = l_itr.next();
+                        if (l_p.getOwnedArmies() > 0) {
+                            l_counter = l_counter + l_p.getOwnedArmies();
+                        }
+                    }
+
+                    //Case when pool has at least 1 army left
+                    //Gets Orders until all pool is consumed for turn
+                    while (l_counter > 0){
+                        //get Orders
+                        for (Player p : d_Players) {
+                            p.issueOrder();
+                        }
+                        //Check for Pool
+                        Iterator<Player> itr = d_Players.listIterator();
+                        l_counter = 0;
+                        while(itr.hasNext()) {
+                            Player l_p = itr.next();
+                            if (l_p.getOwnedArmies() > 0) {
+                                l_counter = l_counter + l_p.getOwnedArmies();
+                            }
+                        }
+                        //If pool is consumed
+                        //Execute current Orders
+                        if(l_counter == 0){
+                            int l_count = 0;
+                            for (Player l_p : d_Players) {
+                                Queue<Order> l_temp = l_p.getD_orderList();
+                                l_count = l_count +l_temp.size();
+                            }
+
+                            if(l_count == 0){
+                                System.out.println("Orders already executed!");
+                            }
+                            else{
+                                System.out.println("Total Orders  :" + l_count);
+                                while (l_count != 0) {
+                                    for (Player l_p : d_Players) {
+                                        Queue<Order> l_temp = l_p.getD_orderList();
+                                        if (l_temp.size() > 0) {
+                                            Order l_toRemove = l_p.next_order();
+                                            l_toRemove.execute();
+                                        }
+                                    }
+                                    l_count--;
+                                }
+
+                                //Flush Owned Cards
+                                for(Player l_p : d_Players) {
+                                    l_p.flushNegotiators();
+                                }
+
+                                //Check if any Player Won
+                                for (Player l_p : d_Players){
+                                    if(l_p.getOwnedCountries().size() == d_Map.getCountries().size()){
+                                        System.out.println(l_p.getPlayerName()+" has Won the Game!");
+                                        d_LogEntry.setMessage(l_p.getPlayerName()+" has Won the Game!");
+                                        winner.put(gameNumber, l_p.getPlayerName());
+                                    }
+                                }
+
+                                //check if any player needs to be removed as of losing all territories
+                                for (Player l_p : d_Players){
+                                    if(l_p.getOwnedCountries().size() == 0){
+                                        System.out.println(l_p.getPlayerName()+" has lost all its territories and is no longer part of the game");
+                                        d_LogEntry.setMessage(l_p.getPlayerName()+" has lost all its territories and is no longer part of the game");
+                                        d_Players.remove(l_p);
+                                    }
+                                }
+                        }
+                    }
+                }
             }
         }
+    }
+        //return winner;
+        TournamentResultView tournamentResultView = new TournamentResultView();
+        tournamentResultView.displayTournamentResult(winner, mapFiles);
     }
 
     /**
@@ -302,5 +387,17 @@ public class TournamentEngine extends GameEngine{
             }
         }
         return true;
+    }
+
+    /**
+     * assigns default reinforcements to each player based on Countries owned.
+     * @param p_cmd GameEngine ref from main to get track of players
+     */
+    public void assignEachPlayerReinforcements(GameEngine p_cmd){
+        Iterator<Player> itr = p_cmd.d_Players.listIterator();
+        while(itr.hasNext()) {
+            Player p = itr.next();
+            AssignReinforcement.assignReinforcementArmies(p);
+        }
     }
 }
