d53a2a0726ff3b2a9c81069725b9b6daccfa2eaf
mindeaarthi1111 <mindeaarthi376@gmail.com>
2021-04-14 10:01:31 +0530

    advance and attack changes

--- src/main/java/model/AggresivePlayer.java
+++ src/main/java/model/AggresivePlayer.java
@@ -8,9 +8,9 @@ import java.util.Random;
  * @author  Aarthi
  */
 public class AggresivePlayer extends PlayerStrategy{
+    private int d_OrderVal,d_maxArmies;
+    CountryDetails d_StrongestCountry,d_DefendingCountry,d_MoveFromCountry;
 
-    private int d_OrderVal;
-    CountryDetails d_StrongestCountry,d_DefendingCountry;
     /**
      * default Constructor for PlayerStrategy
      *
@@ -21,6 +21,7 @@ public class AggresivePlayer extends PlayerStrategy{
         super(p_player, p_map);
         d_StrongestCountry = null;
         d_DefendingCountry = null;
+        d_maxArmies = 0;
     }
 
 
@@ -49,11 +50,11 @@ public class AggresivePlayer extends PlayerStrategy{
     {
         for(CountryDetails l_neighborCountry : d_StrongestCountry.getNeighbours().values())
         {
-           if(!this.d_Player.getOwnedCountries().containsKey(l_neighborCountry))
-           {
+            if(!this.d_Player.getOwnedCountries().containsKey(l_neighborCountry))
+            {
                 d_DefendingCountry = l_neighborCountry;
                 return  d_DefendingCountry;
-           }
+            }
         }
 
         //if no neighbors found to attack
@@ -71,48 +72,83 @@ public class AggresivePlayer extends PlayerStrategy{
         return d_StrongestCountry;
     }
 
+    /**
+     * This function moves the armies to the strongest country
+     *to centralize the forces.
+     * @return source country for advance
+     */
     protected CountryDetails toMoveFrom()
     {
-        //TODO
-        return d_StrongestCountry;
+        int l_maxArmies =0;
+        findStrongestCountryDetails();
+
+        for(CountryDetails l_neighborCountry : d_StrongestCountry.getNeighbours().values())
+        {
+            if(this.d_Player.getOwnedCountries().containsKey(l_neighborCountry))
+            {
+                int l_currentCountryArmies = l_neighborCountry.getNumberOfArmies();
+                if(l_currentCountryArmies > l_maxArmies) {
+                    l_maxArmies = l_currentCountryArmies;
+                    d_MoveFromCountry = l_neighborCountry;
+                    d_maxArmies = l_maxArmies;
+                }
+            }
+        }
+
+        if(l_maxArmies == 0)
+            return null;
+        else
+            return d_MoveFromCountry;
     }
 
     /**
      *  This function  sets  the randomOrderValue which can be 0,1,2
      *  These values 0,1,2 will be used for creating orders
      */
-    public void setRandomOrderValue(int p_randomValue)
+    public void setRandomOrderValue()
     {
-        d_OrderVal = p_randomValue;
+        Random l_random =new Random();
+        d_OrderVal = l_random.nextInt(3);
     }
 
     @Override
     public Order createOrder() {
 
-        CountryDetails l_attackingCountry,l_defendingCountry;
-        l_attackingCountry = toAttackFrom();
-        l_defendingCountry = toAttack();
-        Random l_random = new Random();
+        CountryDetails l_attackingCountry,l_defendingCountry,l_moveFromCountry;
+        l_attackingCountry = toAttackFrom(); //strongest country
+        l_defendingCountry = toAttack();     //neighbor country not belonging to player
+        l_moveFromCountry  = toMoveFrom();   //neighbor of strongest country with max armies
 
+        Random l_random = new Random();
+        setRandomOrderValue();
         switch(d_OrderVal) {
             case 0:
-            if (l_random.nextInt(5) != 0) {
-                return new Deploy(d_Player, d_StrongestCountry.getCountryId(), l_random.nextInt(20));
-            }
-            break;
+                int l_reinforceArmies = l_random.nextInt(d_Player.getOwnedArmies());
+                if (l_reinforceArmies != 0) {
+                    return new Deploy(d_Player, d_StrongestCountry.getCountryId(), l_random.nextInt(20));
+                }
+                else
+                    System.out.println("Invalid value for deploying reinforcement armies");
+                return null;
 
             case 1:
-            //create attack Order
-            if(l_defendingCountry!=null)
-              return new Advance(d_Player, l_attackingCountry.getCountryId(), l_defendingCountry.getCountryId(), l_random.nextInt(6), d_DefendingCountry.getOwnerPlayer());
-            else
-                System.out.println("Neighbor does not exist for this country");
-            break;
+                //create attack Order
+                if(l_defendingCountry!=null) {
+                    return new Advance(d_Player, l_attackingCountry.getCountryId(), l_defendingCountry.getCountryId(), l_random.nextInt(l_attackingCountry.getNumberOfArmies()), d_DefendingCountry.getOwnerPlayer());
+                }
+                else
+                    System.out.println("Neighbor does not exist for this country");
+                return null;
+
 
             case 2:
-            //move armies
+                //move maximum armies from one country to strongest country
+                if(l_moveFromCountry!=null)
+                    return new Advance(d_Player,l_moveFromCountry.getCountryId(),l_attackingCountry.getCountryId(),d_maxArmies,l_attackingCountry.getOwnerPlayer());
+                else
+                    return null;
         }
         return null;
-
     }
+
 }
