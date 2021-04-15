9c3e5c80e9b6f585af8b0bb429fcb6aa64366714
yashs <yashs@LAPTOP-ORKMKVKU>
2021-04-15 17:33:24 +0530

    cc

--- src/test/java/model/TestCheaterPlayer.java
+++ src/test/java/model/TestCheaterPlayer.java
@@ -14,7 +14,7 @@ public class TestCheaterPlayer {
 	
 	RunGameEngine d_RunGame;
 	GameMap d_Map;
-	GameData game;
+	GameData d_Game;
 	Player d_Player1, d_Player2;
 	PlayerStrategy d_Pg1, d_Pg2;
 	
@@ -27,12 +27,12 @@ public class TestCheaterPlayer {
     	d_RunGame= new RunGameEngine();
     	d_Player1= new Player("p1");
     	d_Player2= new Player("p2");
-    	game= new GameData();
+    	d_Game= new GameData();
     	d_Map=d_RunGame.loadMap("demo.map");
     	d_Pg1 = new CheaterPlayer(d_Player1,d_Map);
-        game.getPlayers().add(d_Player1);
+        d_Game.getPlayers().add(d_Player1);
         d_Pg2 = new CheaterPlayer(d_Player2,d_Map);
-        game.getPlayers().add(d_Player2);
+        d_Game.getPlayers().add(d_Player2);
         
         d_Player1.getOwnedCountries().put("one", d_Map.getCountries().get("one"));
         d_Player1.getOwnedCountries().put("two", d_Map.getCountries().get("two"));
